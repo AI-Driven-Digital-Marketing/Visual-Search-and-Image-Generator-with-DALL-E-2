0b3310e8918ea49317ea2be1a6322620c62807e3
Rayden Xu <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-27 15:25:01 -0400

    Update AIGC-Image.py

--- pages/AIGC-Image.py
+++ pages/AIGC-Image.py
@@ -7,23 +7,29 @@ import numpy as np
 
 st.write('## AIGC Image')
 
-st.image(
+# st.image(
+#     "src/data.jpeg",
+#     caption='Query the Data',
+#      use_column_width  = 'always',
+# )
+
+# input GUI for user
+_,col1,_ = st.columns([1,8,1])
+_,col2,_ = st.columns([1,8,1])
+col1, col2 = st.columns(2,gap = "medium")
+
+with col1:
+    st.markdown('#### Original Shirt')
+    st.image(
     "src/data.jpeg",
-    caption='Query the Data',
+    caption='Dalle Shirt',
      use_column_width  = 'always',
-)
-
-    # # input GUI for user
-    # _,col1,_ = st.columns([1,8,1])
-    # _,col2,_ = st.columns([1,8,1])
-    # #col1, col2 = st.columns(2,gap = "medium")
-
-    # with col1:
-
-function_selection = st.selectbox('Select the function here:',
+    )
+with col2:
+    function_selection = st.selectbox('Select the function here:',
                         [ 'Q1','Q2','Q3']
                         )
-st.markdown('#### Features')
+    st.markdown('#### Features')
         if function_selection == 'Q1':
             '''Find customers who have returned items more than 20% more often than the average customer returns for a
             store in a given state for a given year.'''
