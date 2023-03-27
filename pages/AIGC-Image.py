c4f3bc8f1328b9b349c87971cc1315117d718e3c
Rayden Xu <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-27 15:31:44 -0400

    Update AIGC-Image.py

--- pages/AIGC-Image.py
+++ pages/AIGC-Image.py
@@ -1,12 +1,11 @@
 import streamlit as st
 # import os
 # import openai
-import torch
+# import torch
 import numpy as np
 
 
 st.write('## AIGC Image')
-
 st.image(
     "src/test1data.jpeg",
     caption='Query the Data',
