9a4bb83d458bff9daaa54267eb9fa66cfc522954
Rayden Xu <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-27 18:06:46 -0400

    Update AIGC-Image.py

--- pages/AIGC-Image.py
+++ pages/AIGC-Image.py
@@ -12,35 +12,3 @@ st.image(
      use_column_width  = 'always',
 )
 
-# input GUI for user
-_,col1,_ = st.columns([1,8,1])
-_,col2,_ = st.columns([1,8,1])
-col1, col2 = st.columns(2,gap = "medium")
-
-with col1:
-    st.markdown('#### Original Shirt')
-    st.image(
-    "src/test1.jpg.jpeg",
-    caption='Dalle Shirt',
-     use_column_width  = 'always',
-    )
-with col2:
-    form = st.form(key='my_form')
-    form.text_input(label='Enter some text')
-    submit_button = form.form_submit_button(label='Submit')
-
-    
-    
-#     st.markdown('#### Features')
-#     if function_selection == 'Q1':
-#         '''Find customers who have returned items more than 20% more often than the average customer returns for a
-#         store in a given state for a given year.'''
-#         form = st.form(key='Q1-form')
-#         prompt = form.text_input('year',
-#                                         min_value=1900,
-#                                         max_value=2100,
-#                                         value  = 2000,
-#                                         help = 'Input value not in range.(Range: 1900~2100)')
-#         submit = form.form_submit_button('Submit')  
-#     else:
-#         pass
