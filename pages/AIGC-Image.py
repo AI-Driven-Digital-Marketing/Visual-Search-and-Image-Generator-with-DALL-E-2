5a68652b2e6909ac9bcb3ade5f2f4a944d77f5a2
Rayden Xu <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-27 18:07:23 -0400

    Update AIGC-Image.py

--- pages/AIGC-Image.py
+++ pages/AIGC-Image.py
@@ -1,14 +1,10 @@
 import streamlit as st
-# import os
-# import openai
-# import torch
+import os
+import openai
+import torch
 import numpy as np
 
 
 st.write('## AIGC Image')
-st.image(
-    "src/test1data.jpeg",
-    caption='Query the Data',
-     use_column_width  = 'always',
-)
+
 
