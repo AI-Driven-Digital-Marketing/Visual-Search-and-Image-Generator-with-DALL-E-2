6603090fe47f49af4c424c11de46c51740bab711
Rayden Xu <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-27 15:21:36 -0400

    Update AIGC-Image.py

--- pages/AIGC-Image.py
+++ pages/AIGC-Image.py
@@ -6,11 +6,12 @@ import numpy as np
 
 
 st.write('## AIGC Image')
-    st.image(
-        "src/data.jpeg",
-        caption='Query the Data',
-         use_column_width  = 'always',
-    )
+
+st.image(
+    "src/data.jpeg",
+    caption='Query the Data',
+     use_column_width  = 'always',
+)
 
     # # input GUI for user
     # _,col1,_ = st.columns([1,8,1])
@@ -19,17 +20,19 @@ st.write('## AIGC Image')
 
     # with col1:
 
-# function_selection = st.selectbox('Select the function here:',
-#                         [ 'Q1','Q2','Q3']
-#                         )
-# st.markdown('#### Features')
-# if function_selection == 'Q1':
-#     '''Find customers who have returned items more than 20% more often than the average customer returns for a
-# store in a given state for a given year.'''
-#     form = st.form(key='Q1-form')
-#     prompt = form.text_input('year',
-#                                     min_value=1900,
-#                                     max_value=2100,
-#                                     value  = 2000,
-#                                     help = 'Input value not in range.(Range: 1900~2100)')
-#     submit = form.form_submit_button('Submit')  
+function_selection = st.selectbox('Select the function here:',
+                        [ 'Q1','Q2','Q3']
+                        )
+st.markdown('#### Features')
+    if function_selection == 'Q1':
+        '''Find customers who have returned items more than 20% more often than the average customer returns for a
+    store in a given state for a given year.'''
+        form = st.form(key='Q1-form')
+        prompt = form.text_input('year',
+                                        min_value=1900,
+                                        max_value=2100,
+                                        value  = 2000,
+                                        help = 'Input value not in range.(Range: 1900~2100)')
+    submit = form.form_submit_button('Submit')  
+    else:
+     pass
