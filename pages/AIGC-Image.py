4675267d7ce1c9cff0050c36dd48a2149bde282c
Rayden Xu <102635182+Rayden-Xu@users.noreply.github.com>
2023-03-27 15:30:47 -0400

    Update AIGC-Image.py

--- pages/AIGC-Image.py
+++ pages/AIGC-Image.py
@@ -7,16 +7,16 @@ import numpy as np
 
 st.write('## AIGC Image')
 
-# st.image(
-#     "src/data.jpeg",
-#     caption='Query the Data',
-#      use_column_width  = 'always',
-# )
+st.image(
+    "src/test1data.jpeg",
+    caption='Query the Data',
+     use_column_width  = 'always',
+)
 
 # input GUI for user
-_,col1,_ = st.columns([1,8,1])
-_,col2,_ = st.columns([1,8,1])
-col1, col2 = st.columns(2,gap = "medium")
+# _,col1,_ = st.columns([1,8,1])
+# _,col2,_ = st.columns([1,8,1])
+# col1, col2 = st.columns(2,gap = "medium")
 
 with col1:
     st.markdown('#### Original Shirt')
@@ -26,16 +26,10 @@ with col1:
      use_column_width  = 'always',
     )
 with col2:
-#     function_selection = st.selectbox('Select the function here:',
-#                         [ 'Q1','Q2','Q3']
-#                         )
-#     form = st.form(key='Q1-form')
-    prompt = form.text_input('year',
-                                    min_value=1900,
-                                    max_value=2100,
-                                    value  = 2000,
-                                    help = 'Input value not in range.(Range: 1900~2100)')
-    submit = form.form_submit_button('Submit')  
+    form = st.form(key='my_form')
+    form.text_input(label='Enter some text')
+    submit_button = form.form_submit_button(label='Submit')
+
     
     
 #     st.markdown('#### Features')
