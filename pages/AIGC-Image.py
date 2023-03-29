c44aeedc9e9d694dcbcd3a78a84b34976fca7697
Rayden X <xu.rund@northeastern.edu>
2023-03-29 12:13:52 -0400

    1234

--- pages/AIGC-Image.py
+++ pages/AIGC-Image.py
@@ -7,9 +7,8 @@ import requests
 from io import BytesIO
 
 # OpenAI API
-openai.organization = "org-hZCjhqvXmAGGiwqIXIzozujs"
-openai.api_key = "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"
-
+openai.organization = st.secrets["org"]
+openai.api_key = st.secrets["openaikey"]
 
 # Tittl 
 st.write('## AIGC Image')
@@ -27,43 +26,30 @@ with col1:
 
 with col2:
     form = st.form(key='my_form')
-    form.text_input(label='Customerize your creative!')
+    input_prompt = form.text_input(label='Customerize your creative!')
     submit_button = form.form_submit_button(label='Submit')
 
-if color == 'White' and submit_button:
+color_dict = {'White': 'src/white.png', 'Black': 'src/black.png', 'Gary': 'src/gary.png'}
+mask_dict = {'White': 'src/whitemask.png', 'Black': 'src/blackmask.png', 'Gary': 'src/garymask.png'}
+st.image(
+color_dict[color],
+caption='Dalle Shirt',
+width= 500,
+)
+
+if  submit_button:
     st.write('You selected White.')
-    st.image(
-    "src/white.png",
-    caption='Dalle Shirt',
-    width= 500,
+    response = openai.Image.create_edit(
+    image=open(color_dict[color], "rb"),
+    mask=open(mask_dict[color], "rb"),
+    prompt=input_prompt,
+    n=1,
+    size="1024x1024"
     )
-elif color == 'Black' and submit_button:
-    st.write('You selected Black.')
-    st.image(
-    "src/black.png",
-    caption='Dalle Shirt',
-    width= 500,
-    )
-elif color == 'Gary' and submit_button:
-    st.write('You selected Gary.')
-    st.image(
-    "src/gary.png",
-    caption='Dalle Shirt',
-    width= 500,
-    )
-else:
-    st.write("You need select your creative background.")    
+    image_url = response['data'][0]['url']
+    response = requests.get(image_url)
+    img = Image.open(BytesIO(response.content))
+    img
 
 
 # Report body
-response = openai.Image.create_edit(
-  image=open("src/white.png", "rb"),
-  mask=open("src/whitemask.png", "rb"),
-  prompt="A brutalism building by the sea.",
-  n=1,
-  size="1024x1024"
-)
-image_url = response['data'][0]['url']
-response = requests.get(image_url)
-img = Image.open(BytesIO(response.content))
-img
